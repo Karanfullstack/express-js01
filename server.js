cadc62a750ba6863f6bf7523d2b35ecfd3f1ebd4
Karan <karan@karans-air.home>
2023-06-22 16:57:35 +0200

    static middleware

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname)+ '/index.html');
+// });
+
+// app.get('/about', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname)+ '/about.html');
+// });
+
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname)+ '/index.html');
-});
-
-app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname)+ '/about.html');
-});
-
-
 
 
 
