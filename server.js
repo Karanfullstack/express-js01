7f0ea54c5c79a0e4cfdd1c6c72d182740fa45d54
Karan <karan@karans-air.home>
2023-06-22 17:43:57 +0200

    ejs template engine

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
+const {log} = require("console");
 const express = require("express");
 const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+console.log(app.get("views"));
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname)+ '/index.html');
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "HomePage",
+  });
 });
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname)+ '/about.html');
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "AboutPage",
+  });
 });
 
-app.get('/download', (req, res)=>{
-    res.download(path.resolve(__dirname) + '/about.html');
-})
+app.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname) + "/about.html");
+});
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-
-
-
-
-
