edcb78edb37ec9934b3e3ca22d92f28ff0c6fe4d
Karan <karan@karans-air.home>
2023-06-22 16:47:24 +0200

    bulding server to send static file

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname)+ '/index.html');
+});
+
+app.get('/about', (req, res)=>{
+    res.sendFile(path.resolve(__dirname)+ '/about.html');
+});
+
+
+
+
+
+
 
